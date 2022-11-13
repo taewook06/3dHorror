0c6e31f4a1d5ee89d445620864db36dae1a75914
taewook06 <109446725+taewook06@users.noreply.github.com>
2022-11-13 09:33:06 +0900

    `23

--- Assets/Horror/Script/HorrorEvil.cs
+++ Assets/Horror/Script/HorrorEvil.cs
@@ -2,31 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HorrorBody : MonoBehaviour
+public class HorrorEvil : MonoBehaviour
 {
     bool HorrorEvent = false;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
+
     // Update is called once per frame
     void Update()
     {
         if (HorrorEvent == true)
         {
-            transform.Translate(0, 0.01f, -0.0036f);
-            Destroy(gameObject, 9f);
-          
-        }             
-    }   
+            transform.Translate(0f, 0f, 0.05f);
+            Destroy(gameObject, 7f);            
+        }
+    }
     void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player")
+        if(other.transform.tag == "Player")
         {
             HorrorEvent = true;
-            gameObject.GetComponent<AudioSource>().Play();
         }
-       
     }
 }
